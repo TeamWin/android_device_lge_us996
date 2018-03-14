ff6c6eaea5dc38ea9a7596a2e0b96032cb52a850
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-03-14 19:23:53 +0000

    Fixed encryption support, at least for LineageOS

--- omni_h990.mk
+++ omni_h990.mk
@@ -17,3 +17,4 @@ PRODUCT_MODEL := LG-H990
 PRODUCT_NAME := omni_h990
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_COPY_FILES += device/lge/$(PRODUCT_DEVICE)/recovery.fstab:recovery/root/etc/twrp.fstab
