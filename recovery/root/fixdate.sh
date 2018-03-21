2528e5d4ffeafa5354ae8d4f88b2b486bb11ba9e
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-03-21 15:55:11 -0500

    Tweaking fixup script since stock time still acting up

--- recovery/root/fixdate.sh
+++ recovery/root/fixdate.sh
@@ -7,11 +7,13 @@ until pids=$(pidof recovery)
 do
     sleep 1
 done
-sleep 2
+sleep 3
 
 export year=`date +%Y`
 if [ $year -lt 2000 ]
 then
 export year=`expr $year + 46`
 date $(date +%m%d%H%M)$year
+touch /dateadjusted
+else touch /datenotadjusted
 fi
