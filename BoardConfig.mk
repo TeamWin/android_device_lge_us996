a6ea8a192183b6f134a4969fa320c6b200e47fd8
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-04-11 05:44:40 -0500

    Removed Exfat from kernel, using fuse module instead

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,8 @@ TW_CLOCK_OFFSET := -1451606400
 #H918 TW_CLOCK_OFFSET := -1385672400
 
 
-# exFAT drivers included in the kernel
-TW_NO_EXFAT_FUSE := true
+# exFAT drivers no longer included in the kernel
+TW_NO_EXFAT_FUSE := false
 
 # No love for the wicked (device ships with M)
 TW_EXCLUDE_SUPERSU := true
