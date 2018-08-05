7d739a5eb1d59f80a6bebbf43d2c2f2d8aa6cdea
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-08-04 21:23:11 -0500

    Disable offset switch and rework datefix script, some credit for an idea for this goes to @shunjou on github

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,8 +76,8 @@ TW_IGNORE_MISC_WIPE_DATA := true
 
 #Bottom
 #TW_H_OFFSET := -35
-
-TW_CLOCK_OFFSET := -1451606400
+# Lineage 14's era is mostly over, making stock right and lin15 more correct
+#TW_CLOCK_OFFSET := -1451606400
 #US996 offset 46 years
 #H918 TW_CLOCK_OFFSET := -1385672400
 
